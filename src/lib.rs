5131bfb848a74e92ca5f51e29ab986c4568a22b3
Adam Avramov <adam@hack.bg>
2021-04-06 01:05:47 +0300

    refactor: neater output

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,16 @@
 
 macro_rules! kukumba {
 
-    ($(
-        #[$unit:ident]
-        $( $($op:ident $desc:literal)+ { $($stmt:stmt)* } )*
-    )*) => {
+    (
+        $(
+            #[$unit:ident]
+            $( $($op:ident $desc:literal)+ { $($stmt:stmt)* } )*
+        )*
+    ) => {
         $(#[test] fn $unit () {
             $(
-                println!("");
-                $(println!("{} {}", stringify!($op), $desc);)+
+                $(print!("\n  {} {}", stringify!($op), $desc);)+
+                print!(": ");
                 $($stmt;)*
             )*
         })*
